79798f6b81dc28fab3c5480af19f8a7c175b6931
albe.fox <albedo@foxmail.com>
2019-06-13 21:53:58 +0800

    shell_init

--- wordpress-deploy.sh
+++ wordpress-deploy.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 #
 #author:OldFox
-#email:2019/05/30
+#email:albedo@foxmail.com
 #date:2019/05/30
 #usage:install wordpress in LNMP
 #
 
-#download package
-printf "正在下载"
-printf  "."
-printf "..\r"
+function intall_nginx(){
+
+}
+
+
+function install_php(){
+
+}
+
+function install_mysql(){
+
+}
+
+function install_wordpress(){
 wget https://wordpress.org/latest.tar.gz &>/dev/null
 if [ ! $? -eq 0 ];then
 	echo "下载包失败，请自行准备包"	
 	exit
 fi
-echo "下载成功"
-
-echo "解压包."
 tar xf latest.tar.gz -C ./
 mkdir /www.test.com.bak
 mv /usr/share/nginx/html/* /www.test.com.bak/
@@ -25,13 +32,14 @@ mv ./wordpress/* /usr/share/nginx/html/
 
 echo "创建数据库"
 systemctl start mysqld
-mysql -uroot -p123 -e "create database wordpress"
+mysql --login-path=orginal -e "create database wordpress"
 
 echo "准备配置文件"
 cp /usr/share/nginx/html/{wp-config-sample.php,wp-config.php}
 sed -i 's/database_name_here/wordpress/' /usr/share/nginx/html/wp-config.php
 sed -i 's/username_here/root/' /usr/share/nginx/html/wp-config.php
 sed -i 's/password_here/123/' /usr/share/nginx/html/wp-config.php
+}
 
-
+##main
 
