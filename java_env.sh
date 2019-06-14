227fe68c9acfb01ac2f7a5d074c222240d9f5789
albe.fox <albedo@foxmail.com>
2019-06-14 21:53:41 +0800

    shell_init

---
+++ java_env.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# author: oldfox
+# email: albedo@foxmail.com
+# date: 20190614
+# usage: deploy jdk
+#
+
+java_version=$( java --version | awk -F'[ .]+' 'NR==1{ print $2}')
+if [ ! -z $java_version ];then
+        if [ $java_version -lt 9 ];then
+                yum -y remove java*
+		yum -y install java-latest-openjdk.x86_64
+        fi
+else
+	yum -y install java-latest-openjdk.x86_64
+fi
+
+#set java environment
+cat <<-EOF  >>/etc/profile
+JAVA_HOME=/usr/lib/jvm/java-12-openjdk-12.0.1.12-1.rolling.el7.x86_64
+JRE_HOME=\$JAVA_HOME/jre
+CLASS_PATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar:\$JRE_HOME/lib
+PATH=\$PATH:\$JAVA_HOME/bin:\$JRE_HOME/bin
+export JAVA_HOME JRE_HOME CLASS_PATH PATH
+EOF
+source /etc/profile
+env | grep -i java
---
+++ pycharm_install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# author: oldfox
+# email: albedo@foxmail.com
+# date: 20190614
+# usage: deploy pycharm
+#
+wget https://download.jetbrains.8686c.com/python/pycharm-community-2019.1.3.tar.gz
+tar xf pycharm-community-2019.1.3.tar.gz
+if [ ! -d /opt ];then
+	mkdir /opt
+fi
+yum -y install java-latest-openjdk.x86_64
+mv pycharm-community-2019.1.3 /opt/
+cd /opt/pycharm-community-2019.1.3/bin
+#需要graphics environment，图形化环境
+./pycharm.sh
